77cb2e674249d6126692c2ea3a12a0e98449f98a
Mr. steal your set#8959 <125122145+scyllastrikes@users.noreply.github.com>
2023-07-28 10:58:11 +0100

    added features + styling

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let pwdBtn = document.getElementById("pwd-btn")
 let passwordEl =document.getElementById("password-el")
-
+let pwdI= document.getElementById("pwd-i")
+let lenEl=document.getElementById("len-el")
+pwdI.value=12
+pwdI.addEventListener('input', () => {
+    const selectedValue = pwdI.value;
+    lenEl.textContent = `Password lenght: ${selectedValue}`;
+    console.log(pwdI.value)
+    return pwdI.value
+  });
 pwdBtn.addEventListener('click', function(){
     
 })
