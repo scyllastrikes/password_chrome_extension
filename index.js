32730919cdab9e07694763ffac34aee4da82d088
Mr. steal your set#8959 <125122145+scyllastrikes@users.noreply.github.com>
2023-08-17 19:16:25 +0100

    saves current url password

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const pwdI= document.getElementById("pwd-i")
 const lenEl=document.getElementById("len-el")
 const pwd= document.getElementById("pwd")
 const saveBtn=document.getElementById("save-btn")
+const displayBtn=document.getElementById("display-btn")
 pwdI.value=12
+checkpwds()
+saveBtn.style.display="none"
 pwdI.addEventListener('input', () => {
     const selectedValue = pwdI.value
     lenEl.textContent = `Password lenght: ${selectedValue}`
@@ -12,31 +15,54 @@ pwdI.addEventListener('input', () => {
   })
 
 pwdBtn.addEventListener('click', function(){
+  checkpwds()
+  saveBtn.style.display="block"
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?';
   let password = ''
   console.log()
+
   for (let i = 0; i < pwdI.value; i++) {
     const randomIndex = Math.floor(Math.random() * chars.length);
     password += chars.charAt(randomIndex)
   }
+
   pwd.textContent=password
-  
 })
 saveBtn.addEventListener('click',()=>{
-  const pwdList = localStorage.getItem("Password")
+  checkpwds()
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    let url =tabs[0].url
+  const pwdList = localStorage.getItem(url)
   const storedList = JSON.parse(pwdList)
-  let test=["sample"]
-  test.push(storedList)
   let x=[]
+
   if(pwd.textContent!==""){
-  if ( pwdList == null) {
+  if ( pwdList === null) {
     x.push(pwd.textContent)
     const myListJSON = JSON.stringify(x);
-    localStorage.setItem("Password", myListJSON)
+    localStorage.setItem(url, myListJSON)
    }
+
   else if (pwd.textContent!==storedList[storedList.length-1]){
     x.push(storedList)
     x.push(pwd.textContent)
     const myListJSON = JSON.stringify(x);
-    localStorage.setItem("Password", myListJSON)
-  }}}) 
+    localStorage.setItem(url, myListJSON)
+  }}})}) 
+
+
+function checkpwds(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    let url =tabs[0].url
+  const pwdList = localStorage.getItem(url)
+  if ( pwdList !== null) {
+    displayBtn.style.display="block"
+   }
+  else{
+    displayBtn.style.display="none"
+  }
+})}
+
+ 
+
+//.addEventListener('click',()=>{
